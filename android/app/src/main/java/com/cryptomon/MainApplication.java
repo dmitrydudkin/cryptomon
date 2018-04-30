f756dbae12ae9b263b77511c7d4f74ac9a3252b1
dimondudkin <dimondudkin@gmail.com>
2018-04-30 12:34:39 +0300

    Added fabric.io

--- android/app/src/main/java/com/cryptomon/MainApplication.java
+++ android/app/src/main/java/com/cryptomon/MainApplication.java
@@ -3,6 +3,7 @@ package com.cryptomon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.smixx.fabric.FabricPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FabricPackage(),
             new RNFirebasePackage(),
             new ReactNativeConfigPackage()
       );
